2ecb67c57fe64a18d598a2edc56330b0c5742b97
Bkfreedom <88872561+Blake556@users.noreply.github.com>
2023-01-05 10:45:09 -0700

    v6 - can display 1 song from spotify api

--- app.js
+++ app.js
@@ -15,11 +15,12 @@ app.use(express.static('public'))
 //const redirect_uri = 'http://localhost:3020/callback';
 
 
-const name = 'blake'
+// app.get("/", function(req, res) {
+//     res.sendFile(__dirname + '/views/index.ejs')
+//})
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + '/index.html')
-    
+app.get('/', function(req, res) {
+    res.render('index')
 })
 
 
@@ -46,16 +47,12 @@ app.post("/", function(req, res) {
         if (!error && response.statusCode === 200) {
             var token = body.access_token;
             //console.log(body)
-            res.sendFile(__dirname + '/index.html')
-        } else {
-            res.sendFile(__dirname + '/fail.html')
-        }
-
-
-        console.log(req.body)
+            ///////////////res.render('index')
+            //res.sendFile(__dirname + './views/index')
+        
         let searchBar = req.body.search
+        console.log(searchBar)
 
-       
         const url = `https://api.spotify.com/v1/search?type=track,artist,album&limit=5&q=${searchBar}`
         //`https://api.spotify.com/v1/search?q=${searchBar}&type=artist&limit=1`
      
@@ -75,27 +72,34 @@ app.post("/", function(req, res) {
 
             console.log()
 
-            let track = body.tracks.items
-            let tracks = track.map(tracks => {
+            let track = body.tracks.items[0].name
+            let artist = body.artists.items[0].name
+            let album = body.albums.items[0].name
+            console.log(track)
+
+            res.render('index', { text: track, song: artist, ball: album})
+
+            // let tracks = track.map(tracks => {
                 
-                return tracks.name
-            })
+            //     return tracks.name
+            // })
 
             
 
             console.log()
 
-            let artist = body.artists.items
-            let artistList = artist.map(artists => {
-                return artists.name
-            })
+            //////let artist = body.artists.items[0].name
+            
+            // let artistList = artist.map(artists => {
+            //     return artists.name
+            // })
 
             console.log()
 
-            let album = body.albums.items
-            let albumList = album.map(album => {
-                return album.name
-            })
+            //let album = body.albums.items
+            // let albumList = album.map(album => {
+            //     return album.name
+            // })
 
             // let trackElement = document.querySelector('.track')
             // trackElement.innerText = track
@@ -113,6 +117,10 @@ app.post("/", function(req, res) {
         //     console.log(tracks)
         //  })
 
+        } else {
+            res.sendFile(__dirname + '/fail.html')
+        }
+
         
         console.log()
 
@@ -124,8 +132,10 @@ app.post("/", function(req, res) {
 
 
     app.get('/', function(req, res) {
-        let header = document.querySelector('.header')
-        console.log(header)
+        // let header = document.querySelector('.header')
+         console.log('hello')
+
+        res.render('index', {text: 'world' || 'song'})
         
     })
 
@@ -146,7 +156,7 @@ app.listen(PORT, function() {
 
 
 
-
+// Big changes switched from bodyparser to installing e.js to 'view' html this is inside the view folder which was created and app.set is rendering it this is so html can be modified and ideally i can pass spotify data to html still working on how to grab specific elements but i think i just figured it out add <% %> whatever in html file to pass the variables view web dev siplified for details
 
 
 
