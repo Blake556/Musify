80ac20f64f716b68e5561d9335c4fedaa2077ab8
Bkfreedom <88872561+Blake556@users.noreply.github.com>
2023-05-23 09:31:34 -0700

    Musify

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.post("/", function(req, res) {
 
 
     request.post(authOptions, function(error, response, body) {
+        
         if (!error && response.statusCode === 200) {
             var token = body.access_token;
            
